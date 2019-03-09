7660b1924cba9f2b517a584e94f126c22c7a2cb8
map0 <vortep@protonmail.com>
2019-03-09 21:59:21 +0200

    initial routing

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// express setup
+const express = require('express')
+const bodyParser = require('body-parser')
+const routes = require('./routes/index')
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', routes)
+
+module.exports = app
--- start.js
+++ start.js
@@ -1,1 +1,6 @@
-//  app starter file
+const app = require('./app');
+
+app.set('port', process.env.PORT || 7878)
+app.listen(app.get('port'), () => {
+  console.log(`Express running on PORT ${app.get('port')}`)
+})
