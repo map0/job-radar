c55c1d9a585a743e8394701580f6c3d25648ab61
map0 <vortep@protonmail.com>
2019-03-11 11:53:05 +0200

    we use pug and have layout page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+// const path = require('path')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('routing works')
+  res.render('layout')
+  // res.sendFile(path.join(__dirname, '../views/pure.html'))
 })
 
 module.exports = router
