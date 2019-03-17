4917bea26c41f6d52ebfe7c6731cfe555331b940
map0 <vortep@protonmail.com>
2019-03-17 14:16:58 +0200

    test out email sending

--- start.js
+++ start.js
@@ -18,3 +18,6 @@ app.set('port', process.env.PORT || 7878)
 app.listen(app.get('port'), () => {
   console.log(`Express running on PORT ${app.get('port')}`)
 })
+
+// TEMP send email
+require('./handlers/mailtrap')
