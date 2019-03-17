16ff84d1d963460eb746b063f8d875c21abb14c1
map0 <vortep@protonmail.com>
2019-03-17 12:52:32 +0200

    we can reset account passwords

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ const passport = require('passport')
 const crypto = require('crypto')
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
-const promisify = require('es6-promisify')
+const { promisify } = require('es6-promisify')
 
 exports.login = passport.authenticate('local', {
   failureRedirect: '/login',
@@ -42,4 +42,48 @@ exports.forgot = async (req, res) => {
   req.flash('success', `You have been emailed a password reset link. ${resetURL}`)
 
   res.redirect('/login')
+}
+
+exports.reset = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }
+  })
+  if (!user) {
+    req.flash('error', 'Password reset is invalid or has expired')
+    return res.redirect('/login');
+  }
+  // if there is a user, show the reset password form
+  res.render('reset', { title: 'Reset your Password' })
+}
+
+exports.confirmedPasswords = (req, res, next) => {
+  if (req.body.password === req.body['password-confirm']) {
+    next()
+    return
+  }
+  req.flash('error', 'Passwords do not match!')
+  res.redirect('back')
+}
+
+exports.update = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }
+  })
+
+  if (!user) {
+    req.flash('error', 'Password reset is invalid or has expired')
+    return res.redirect(302, '/login')
+  }
+
+  const setPassword = promisify((user.setPassword).bind(user))
+  await setPassword(req.body.password)
+  user.resetPasswordToken = undefined
+  user.resetPasswordExpires = undefined
+  const updatedUser = await user.save()
+  const logUser = promisify(req.login)
+  logUser(updatedUser)
+  req.flash('success', '💃 Nice! Your password has been reset! You are now logged in!')
+  res.redirect('/')
 }
--- routes/index.js
+++ routes/index.js
@@ -46,5 +46,9 @@ router.post('/register',
 router.get('/account', authController.isLoggedIn, userControllers.account)
 router.post('/account', catchErrors(userControllers.updateAccount))
 router.post('/account/forgot', catchErrors(authController.forgot))
+router.get('/account/reset/:token', catchErrors(authController.reset))
+router.post('/account/reset/:token',
+  authController.confirmedPasswords,
+  catchErrors(authController.update))
 
 module.exports = router
