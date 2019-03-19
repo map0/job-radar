e6f4258c024d93f98c796b7614bd6c96565241e0
map0 <vortep@protonmail.com>
2019-03-19 13:21:45 +0200

    we can create pins on the map

--- public/javascript/modules/map.js
+++ public/javascript/modules/map.js
@@ -1,23 +1,43 @@
 import axios from 'axios'
 
-const  defaultLat  = 42.6648323
-const  defafultLng = 23.26632
+const defaultLat  = 42.6648323
+const defafultLng = 23.26632
 
 const mapOptions = {
   center: { lat: defaultLat, lng: defafultLng },
-  zoom: 10
+  zoom: 13
 }
 
 function loadPlaces(map, lat = defaultLat, lng = defafultLng) {
+  axios.get(`/api/jobPosts/near?lat=${lat}&lng=${lng}`)
+    .then(res => {
+      const places = res.data;
+      if (!places.length) {
+        alert('no places found!');
+        return;
+      }
+      // create a bounds
+      const bounds = new google.maps.LatLngBounds();
+
+      const markers = places.map(place => {
+        const [placeLng, placeLat] = place.location.coordinates;
+        const position = { lat: placeLat, lng: placeLng };
+        bounds.extend(position);
+        const marker = new google.maps.Marker({ map, position });
+        marker.place = place;
+        return marker;
+      })
+    })
+        // when someone clicks on a marker, show the details of that place
+      // markers.forEach...
 
 }
 
 function makeMap(mapDiv) {
   if (!mapDiv) return
-
   // make our map
   const map = new google.maps.Map(mapDiv, mapOptions)
-  // loadPlaces(map)
+  loadPlaces(map)
 }
 
 export default makeMap
